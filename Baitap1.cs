3c478cd851027f1fbf56ac8e9d5eedf56858ba79
Mihi0912 <148853591+BIT230152@users.noreply.github.com>
2024-08-10 13:18:42 +0700

    Add files via upload

---
+++ Baitap1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Baitap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap mot so nguyen: ");
+            int number = int.Parse(Console.ReadLine());
+
+            double double_number = (double)number;
+
+            Console.WriteLine($"So ban da nhap la {number} va o dang kep la {double_number}");
+        }
+    }
+}
